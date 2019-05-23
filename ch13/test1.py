ff7c06ee557487686dc4415784e6467a5137df9e
Peng Xiao <xiaoquwl@gmail.com>
2019-05-23 23:32:57 +0200

    logging init
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ ch13/test1.py
@@ -0,0 +1,11 @@
+"""
+debug()，info()，warning()，error()，critical()
+"""
+import logging
+
+logging.basicConfig(level='INFO')
+logging.debug('this is debug')
+logging.info('this is info')
+logging.warning('this is warning')
+logging.error('this is erro')
+logging.critical('this is critical')
