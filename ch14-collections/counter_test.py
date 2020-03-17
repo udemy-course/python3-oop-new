a7d3cb8f49b7a37e0f46684b5334f1a4acbdd9ab
Peng Xiao <xiaoquwl@gmail.com>
2020-03-17 23:44:37 +0100

    counter

---
+++ ch14-collections/counter_test.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+word_list = ['is', 'who', 'when', 'it', 'is', 'who', 'is']
+
+c = Counter('afdeofiafeafeieaafdhfasdfelkiencs')
+
+print(c)
+
+c.update('xsiefaefafeafefsesfe')
+
+print(c)
