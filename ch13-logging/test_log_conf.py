06b26e5ecb4b0c4f7ee594bef0c24e909521abee
Peng Xiao <xiaoquwl@gmail.com>
2023-03-01 11:39:51 +0100

    logging config update

--- ch13-logging/test_log_conf.py
+++ ch13-logging/test_log_conf.py
@@ -4,3 +4,7 @@ from logging.config import fileConfig
 fileConfig('log.conf')
 logger = logging.getLogger()
 logger.debug('often makes a very good meal of %s', 'visiting tourists')
+
+# you will see the following output:
+# DEBUG:root:often makes a very good meal of visiting tourists
+# and log file will be created in the current directory called example.log
